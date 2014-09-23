3fd87e2902283bc48ae862bf3e6f46a99eb8648b
Matthieu Baerts <matttbe@gmail.com>
2014-09-23 18:06:37 +0200

    prepare-test: added the possibility to launch the test

--- prepare-test.sh
+++ prepare-test.sh
@@ -13,3 +13,7 @@ cd uitests-$PROJ_NAME
 ant build || exit 1
 cd bin
 adb push uitests-$PROJ_NAME.jar  /storage/sdcard0/uitests-$PROJ_NAME.jar || exit 1
+cd $DIR
+
+read -p "Do you want to launch it? [Y/n] " UI_LAUNCH
+[ "$UI_LAUNCH" != "n" -a "$UI_LAUNCH" != "N" ] && adb shell uiautomator runtest /storage/sdcard0/uitests-$PROJ_NAME.jar -c $PROJ_NAME.LaunchSettings
