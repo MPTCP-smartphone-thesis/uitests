f754128a29c87a29ce35625331b1c90da7c84be4
Matthieu Baerts <matttbe@gmail.com>
2014-09-24 14:59:18 +0200

    Prepare-test: always launch the test at the end

--- prepare-test.sh
+++ prepare-test.sh
@@ -16,9 +16,9 @@ which adb > /dev/null || (echo "ADB not found: add it to your PATH" && exit 1)
 [ "$(adb devices | wc -l)" -lt 3 ] && echo "Device not connected" && exit 1
 
 cd bin
+echo -e "\n\t==== Push the new jar ===="
 adb push uitests-$PROJ_NAME.jar  /storage/sdcard0/uitests-$PROJ_NAME.jar || exit 1
 cd $DIR
 
-echo
-read -p "Do you want to launch it? [Y/n] " UI_LAUNCH
-[ "$UI_LAUNCH" != "n" -a "$UI_LAUNCH" != "N" ] && adb shell uiautomator runtest /storage/sdcard0/uitests-$PROJ_NAME.jar -c $PROJ_NAME.LaunchSettings
+echo -e "\n\t==== Launch the test ===="
+adb shell uiautomator runtest /storage/sdcard0/uitests-$PROJ_NAME.jar -c $PROJ_NAME.LaunchSettings
