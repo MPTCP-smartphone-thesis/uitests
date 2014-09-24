40e4826cad0360aeb93977ea2e62a0d94fbacd0b
Matthieu Baerts <matttbe@gmail.com>
2014-09-24 13:40:58 +0200

    prepare: Check if adb is available and the device is connected

--- prepare-test.sh
+++ prepare-test.sh
@@ -10,9 +10,14 @@ which android > /dev/null || (echo "Android not found: add it to your PATH" && e
 
 cd uitests-$PROJ_NAME
 ant build || exit 1
+
+which adb > /dev/null || (echo "ADB not found: add it to your PATH" && exit 1)
+[ "$(adb devices | wc -l)" -lt 3 ] && echo "Device not connected" && exit 1
+
 cd bin
 adb push uitests-$PROJ_NAME.jar  /storage/sdcard0/uitests-$PROJ_NAME.jar || exit 1
 cd $DIR
 
+echo
 read -p "Do you want to launch it? [Y/n] " UI_LAUNCH
 [ "$UI_LAUNCH" != "n" -a "$UI_LAUNCH" != "N" ] && adb shell uiautomator runtest /storage/sdcard0/uitests-$PROJ_NAME.jar -c $PROJ_NAME.LaunchSettings
