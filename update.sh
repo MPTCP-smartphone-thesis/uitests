d3359a98e8080873e5d3647642221a44fa3d753f
Matthieu Baerts <matttbe@gmail.com>
2014-09-24 16:28:28 +0200

    Fixed update HEAD (I think :-) )

--- update.sh
+++ update.sh
@@ -1,2 +1,3 @@
 git pull --recurse-submodules || git pull -u origin master --recurse-submodules
+./foreach.sh git pull || ./foreach.sh git pull -u origin master
 # git submodule update --remote --recursive
