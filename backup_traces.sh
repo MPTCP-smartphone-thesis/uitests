91eb4bce165323ef658a45ec40a2efecfc2acc90
Matthieu Baerts <matttbe@gmail.com>
2014-11-19 20:20:58 +0100

    save_traces_purge_phone: just backup

--- backup_traces.sh
+++ backup_traces.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
 [ "$1" != "" ] && TRACE_DIR=$1 || TRACE_DIR="~/Thesis/TCPDump"
 
+RC=0
+
 # need to add an entry 'mptcpdata' in ~/.ssh/config and load your SSH key in your SSH Agent (via ssh-add)
 # or use sshpass: rsync $TRACE_DIR --rsh='sshpass -p PASSWORD ssh -l USER' host.example.com:path
 echo "Launch RSync"
-rsync -az $TRACE_DIR mptcpdata:/home/mptcp/smartphone || exit 1
-
-echo "Remove files from the phone"
-adb shell "rm -r /storage/sdcard0/traces*"
+rsync -az $TRACE_DIR mptcpdata:/home/mptcp/smartphone
