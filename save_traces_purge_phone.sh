e5fc1da84cf008a7bb0b628c58e295e6d02d1ede
Quentin De Coninck <quentin.deconinck@student.uclouvain.be>
2014-11-19 13:18:09 +0100

    [ADD] Compression in xz format of traces

--- save_traces_purge_phone.sh
+++ save_traces_purge_phone.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 [ "$1" != "" ] && TRACE_DIR=$1 || TRACE_DIR="~/Thesis/TCPDump"
 
+now=$(date +"%Y_%m_%d_%H_%m_%S")
+
+# compress files
+adb shell "tar -cJf /storage/sdcard0/traces_$now.tar.xz /storage/sdcard0/traces*"
+
 # need to add an entry 'mptcpdata' in ~/.ssh/config and load your SSH key in your SSH Agent (via ssh-add)
 # or use sshpass: rsync $TRACE_DIR --rsh='sshpass -p PASSWORD ssh -l USER' host.example.com:path
 echo "Launch RSync"
