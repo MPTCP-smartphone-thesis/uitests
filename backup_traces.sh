33e85891a878b995cc20cb7d9f642bad37c2fa60
Matthieu Baerts <matttbe@gmail.com>
2014-11-21 18:33:19 +0100

    backup_traces: full command for ssh
    + added options

--- backup_traces.sh
+++ backup_traces.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 [ "$1" != "" ] && TRACE_DIR=$1 || TRACE_DIR="~/Thesis/TCPDump"
+[ "$2" != "" ] && DEST_DIR=$2 || DEST_DIR="/home/mptcp/smartphone"
+[ "$3" != "" ] && SSH_USER=$3 || SSH_USER="mptcpdata"
 
 RC=0
 
 # need to add an entry 'mptcpdata' in ~/.ssh/config and load your SSH key in your SSH Agent (via ssh-add)
 # or use sshpass: rsync $TRACE_DIR --rsh='sshpass -p PASSWORD ssh -l USER' host.example.com:path
 echo "Launch RSync"
-rsync -az $TRACE_DIR mptcpdata:/home/mptcp/smartphone
+rsync -az $TRACE_DIR -e "ssh $SSH_USER:$DEST_DIR"
