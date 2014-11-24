596e61ab4c8d703403f7292fb9434327de39a889
Quentin De Coninck <quentin.deconinck@student.uclouvain.be>
2014-11-24 19:00:59 +0100

    [FIX] create_pcap_when_test_started: little correction for string concatenation

--- create_pcap_when_test_started.sh
+++ create_pcap_when_test_started.sh
@@ -6,5 +6,5 @@
 > /home/mptcp/smartphone/.tcpdump-start 
 while inotifywait -e modify /home/mptcp/smartphone/.tcpdump-start; do
    CURR_APP=$(cat /home/mptcp/smartphone/.tcpdump-start | tail -n 1)
-   tcpdump -i veth3426f38 -w "/home/mptcp/smartphone-server/$CURR_APP .pcap" || echo $! > /home/mptcp/smartphone/.tcpdump-pid
+   tcpdump -i veth3426f38 -w "/home/mptcp/smartphone-server/${CURR_APP}.pcap" || echo $! > /home/mptcp/smartphone/.tcpdump-pid
 done
