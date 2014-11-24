1fd4343373fb6b7c9a151416be35366386acf2e3
Quentin De Coninck <quentin.deconinck@student.uclouvain.be>
2014-11-24 19:13:41 +0100

    [IMP] create_pcap_when_test_started: added a little comment :)

--- create_pcap_when_test_started.sh
+++ create_pcap_when_test_started.sh
@@ -3,6 +3,7 @@
 
 > /home/mptcp/smartphone/.tcpdump-start 
 while inotifywait -e modify /home/mptcp/smartphone/.tcpdump-start; do
+   # The last line of .tcpdump-start contains the name of the application
    CURR_APP=$(cat /home/mptcp/smartphone/.tcpdump-start | tail -n 1)
    tcpdump -i veth3426f38 -w "/home/mptcp/smartphone-server/${CURR_APP}.pcap" || echo $! > /home/mptcp/smartphone/.tcpdump-pid
 done
