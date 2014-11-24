15ef0298ca5669fb06855f4af4b39c4d765bfb4f
Quentin De Coninck <qdeconinck@users.noreply.github.com>
2014-11-24 22:23:12 +0100

    [FIX] stop_pcap_when_test_completed: forgotten $

--- stop_pcap_when_test_completed.sh
+++ stop_pcap_when_test_completed.sh
@@ -3,8 +3,8 @@
 
 > /home/mptcp/smartphone/.tcpdump-stop
 while inotifywait -e modify /home/mptcp/smartphone/.tcpdump-stop; do
-   for pid in $ (cat /home/mptcp/smartphone/.tcpdump-pid); do
-      kill pid
+   for pid in $(cat /home/mptcp/smartphone/.tcpdump-pid); do
+      kill $pid
    done
    rm /home/mptcp/smartphone/.tcpdump-pid
 done
