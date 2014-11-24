468c132db47eab386c8dc542fb1490ca8b1daa36
Quentin De Coninck <quentin.deconinck@student.uclouvain.be>
2014-11-24 19:17:20 +0100

    [IMP] create_pcap_when_test_started: added the interface argument

--- create_pcap_when_test_started.sh
+++ create_pcap_when_test_started.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
-# Usage: ./create_pcap_when_test_started.sh
+# Usage: ./create_pcap_when_test_started.sh [interface]
+
+[ "$1" != "" ] && IF=$1 || IF="veth3426f38"
 
 > /home/mptcp/smartphone/.tcpdump-start 
 while inotifywait -e modify /home/mptcp/smartphone/.tcpdump-start; do
    # The last line of .tcpdump-start contains the name of the application
    CURR_APP=$(cat /home/mptcp/smartphone/.tcpdump-start | tail -n 1)
-   tcpdump -i veth3426f38 -w "/home/mptcp/smartphone-server/${CURR_APP}.pcap" || echo $! > /home/mptcp/smartphone/.tcpdump-pid
+   tcpdump -i $IF -w "/home/mptcp/smartphone-server/${CURR_APP}.pcap" & | echo $! >> /home/mptcp/smartphone/.tcpdump-pid
 done
