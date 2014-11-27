d156af6c9fdf3d2991ae99beb6a402a9311d6cde
Matthieu Baerts <matttbe@gmail.com>
2014-11-27 23:59:58 +0100

    get_app_name.sh: write pkg's name too

--- get_app_name.sh
+++ get_app_name.sh
@@ -2,7 +2,10 @@
 for i in uitests-*; do
     app=${i:8}
     [ -f $i/src/$app/LaunchSettings.java ] || continue
-    name=$(grep "Utils\.openApp" $i/src/$app/LaunchSettings.java | cut -d\" -f2)
-    echo $name
+    [ "$app" = "firefox" -o "$app" = "firefoxspdy" ] && file="$i/src/common/FirefoxCommon.java" || file="$i/src/$app/LaunchSettings.java"
+    name=$(grep "Utils\.openApp" $file | cut -d\" -f2)
+    pkg=$(grep "Utils\.openApp" $file | cut -d\" -f4)
+    echo "$name - $pkg"
     [ -n "$name" ] && echo $name > $i/app_name.txt
+    [ -n "$pkg" ] && echo $pkg > $i/pkg_name.txt
 done
