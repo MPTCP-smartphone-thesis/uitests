ce8d5a4103e88c8769090bdcbd438ac94b94943a
Matthieu Baerts <matttbe@gmail.com>
2014-12-29 03:05:01 +0100

    rm listener: use '*' to remove the mode

--- rm_full_pcap_listener.sh
+++ rm_full_pcap_listener.sh
@@ -10,5 +10,5 @@ chmod 777 "$FILE"
 while inotifywait -e modify "$FILE"; do
    # The last line of .tcpdump-rm contains the file (basename) to delete
    CURR_APP=$(tail -n 1 "$FILE" || continue)
-   rm "${OUT}/${CURR_APP}.pcap"
+   rm "${OUT}/*_${CURR_APP}.pcap"
 done
