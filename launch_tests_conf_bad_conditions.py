06501d2d5e22861f545ddece35fdb087b96948a4
Matthieu Baerts <matttbe@gmail.com>
2014-12-31 13:19:22 +0100

    conf_bad: fixed typo

--- launch_tests_conf_bad_conditions.py
+++ launch_tests_conf_bad_conditions.py
@@ -1,8 +1,10 @@
+import os
+
 if os.path.isfile('launch_tests_conf.py'):
     from launch_tests_conf import *
 
 # Out in another dir
-OUTPUT_DIR += '_bad_conditions'
+OUTPUT_DIR = '~/Thesis/TCPDump_bad_conditions'
 
 # Tests both4 then with some losses or delays
 NETWORK_TESTS = 'both4 both4TCL2p both4TCL5p both4TCL10p both4TCL15p both4TCL20p both4TCD5m both4TCD10m both4TCD25m both4TCD50m both4TCD100m'
