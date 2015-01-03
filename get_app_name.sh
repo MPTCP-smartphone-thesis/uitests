b64d5b86d85018f0c378c715318962ef9a6d76ce
Matthieu Baerts <matttbe@gmail.com>
2015-01-03 22:39:44 +0100

    get_app_name: use pattern to allow new lines
    + print activity name

--- get_app_name.sh
+++ get_app_name.sh
@@ -3,9 +3,12 @@ for i in uitests-*; do
     app=${i:8}
     [ -f $i/src/$app/LaunchSettings.java ] || continue
     [ "$app" = "firefox" -o "$app" = "firefoxspdy" ] && file="$i/src/common/FirefoxCommon.java" || file="$i/src/$app/LaunchSettings.java"
-    name=$(grep "Utils\.openApp" $file | cut -d\" -f2)
-    pkg=$(grep "Utils\.openApp" $file | cut -d\" -f4)
-    echo "$name - $pkg"
+    infos=$(sed -e '/Utils\.openApp(/,/)/!d' $file | tr '\n' ' ')
+    name=$(echo $infos | cut -d\" -f2)
+    pkg=$(echo $infos | cut -d\" -f4)
+    act=$(echo $infos | cut -d\" -f6)
+    echo "$name - $pkg - $act"
     [ -n "$name" ] && echo $name > $i/app_name.txt
     [ -n "$pkg" ] && echo $pkg > $i/pkg_name.txt
+    [ -n "$act" ] && echo $act > $i/act_name.txt
 done
