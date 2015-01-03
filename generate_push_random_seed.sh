724c86fd3cab298bdc9e6b6d442afc8df276bf6e
Matthieu Baerts <matttbe@gmail.com>
2015-01-04 00:30:34 +0100

    generate_push_random_seed: quit if error with dd

--- generate_push_random_seed.sh
+++ generate_push_random_seed.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-dd if=/dev/urandom of=random_seed_orig bs=1 count=800000
+dd if=/dev/urandom of=random_seed_orig bs=1 count=800000 || exit 1
 
 which adb > /dev/null || (echo "ADB not found: add it to your PATH" && exit 1)
 [ "$(adb devices | wc -l)" -lt 3 ] && echo "Device not connected" && exit 1
