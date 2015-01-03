5e142c8215b2389cf14a14cb1d4c2d4f741c6cd1
Matthieu Baerts <matttbe@gmail.com>
2015-01-04 00:29:58 +0100

    generate_push_random_seed: remove file at the end

--- generate_push_random_seed.sh
+++ generate_push_random_seed.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 
 dd if=/dev/urandom of=random_seed_orig bs=1 count=800000
+
 which adb > /dev/null || (echo "ADB not found: add it to your PATH" && exit 1)
 [ "$(adb devices | wc -l)" -lt 3 ] && echo "Device not connected" && exit 1
+
 adb push random_seed_orig  /storage/sdcard0/random_seed_orig
+
+rm random_seed_orig
