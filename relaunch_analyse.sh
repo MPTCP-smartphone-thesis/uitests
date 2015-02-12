41e8eaa1cf99ce5a9b3648d5a2b82bd564424c8b
Matthieu Baerts <matttbe@gmail.com>
2015-02-12 19:19:25 +0100

    Added relaunch_analyse script
    To relaunch analyse with files already cleaned

---
+++ relaunch_analyse.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+test -z "$1" && echo "No dir, exit" && exit 1
+test ! -d "$1" && echo "Dir '$1' doesn't exist, exit" && exit 1
+
+FULLDIR=$1
+DIR=$(basename $FULLDIR)
+CORES=$(grep -c "^processor" /proc/cpuinfo)
+CORES=$(($CORES + $CORES/2)) # we have locks which take time
+
+test ! -d "traces/$DIR" && echo "Dir 'traces/$DIR' doesn't exist in the current dir, exit" && exit 1
+
+rm -rf aggls/$DIR graphs/$DIR stats/$DIR
+echo "$FULLDIR -b 1000 -P -j $CORES -l -C" >> /home/mptcp/smartphone/.analyse-start
