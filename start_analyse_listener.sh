44498a3d3ca909ad590f05a519a9934cc7c79eb3
Matthieu Baerts <matttbe@gmail.com>
2015-02-18 00:17:37 +0100

    start analyse: no need to clean
    Was only needed for old traces

--- start_analyse_listener.sh
+++ start_analyse_listener.sh
@@ -19,8 +19,8 @@ while inotifywait -e modify "$FILE"; do
     LAST_LINE=$(tail -n 1 "$FILE" || echo "UNKNOWN")
     DIR=$(echo $LAST_LINE | awk '{print $1}')
     ARGS=$(echo $LAST_LINE | awk '{$1=""; print $0}')
-    # -c clean, -P keep cleaned traces, -j 4 jobs, -l log to stderr (GnuPlot.py also writes pdf in stdout)
-    test -z "$ARGS" && ARGS="-c -P -j $CORES -l"
+    # -P keep cleaned traces, -j 4 jobs, -l log to stderr (GnuPlot.py also writes pdf in stdout)
+    test -z "$ARGS" && ARGS="-P -j $CORES -l"
     OUT_DIR="pcap/"$(basename $DIR)
     mkdir -p "$OUT_DIR"
     LOG="$OUT_DIR/log_$(date +%Y%m%d_%H%M%S).txt"
