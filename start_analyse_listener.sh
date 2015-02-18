06a22c46ff9d023b8bc1566eeaf3dc0ba2069ea0
Matthieu Baerts <matttbe@gmail.com>
2015-02-18 01:56:29 +0100

    start analyse: server: used the right dir

--- start_analyse_listener.sh
+++ start_analyse_listener.sh
@@ -18,10 +18,14 @@ while inotifywait -e modify "$FILE"; do
 
     # The last line of .analyse-start contains the directory to analyse
     LAST_LINE=$(tail -n 1 "$FILE" || echo "UNKNOWN")
+
     DIR=$(echo $LAST_LINE | awk '{print $1}')
+    SERVER_DIR=$(echo $DIR | sed -e 's#/smartphone/#/smartphone-server/#g')
+
     ARGS=$(echo $LAST_LINE | awk '{$1=""; print $0}')
     # -j 4 jobs
     test -z "$ARGS" && ARGS="-j $CORES"
+
     OUT_DIR="pcap/"$(basename $DIR)
     mkdir -p "$OUT_DIR"
     LOG_NAME="$OUT_DIR/log_$(date +%Y%m%d_%H%M%S)"
@@ -34,7 +38,7 @@ while inotifywait -e modify "$FILE"; do
 
     # For any
     ./analyze.py -i "$DIR" $ARGS >> "$LOG" 2>&1 & # accepts other jobs
-    # For the server ()
-    ./analyze.py -i "$DIR" -p 'server' $ARGS >> "$LOG_SERVER" 2>&1 & # accepts other jobs
+    # For the server (/smartphone-server/, with 'server' prefix)
+    ./analyze.py -i "$SERVER_DIR" -p 'server' $ARGS >> "$LOG_SERVER" 2>&1 & # accepts other jobs
     echo $! >> "$PID"
 done
