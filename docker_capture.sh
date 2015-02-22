f9174f7870690cfeb241ad298bdaa7c634bdc512
Matthieu Baerts <matttbe@gmail.com>
2015-02-22 09:59:04 +0100

    capture: collectd: simply use 127.0.0.1 address

--- docker_capture.sh
+++ docker_capture.sh
@@ -25,7 +25,3 @@ fi
 
 # Other:
 docker start ripe janus ssh-simple openvpn collectd
-
-COLLECTD_IP=$(docker inspect collectd | grep IPAddress | cut -d\" -f4)
-sed -i '/"49155"/ s/.*/      Server "'$COLLECTD_IP'" "49155"/g' /etc/collectd/collectd.conf
-service collectd restart
