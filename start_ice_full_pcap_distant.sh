c1d5fc28f6e982d68e5e4756173f07a2fed09faf
Matthieu Baerts <matttbe@gmail.com>
2015-04-22 12:15:31 +0200

    Remove ice script -> streaming-scripts repo

--- start_ice_full_pcap_distant.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-DEST_FILE="/home/mptcp/smartphone-ice/.tcpdump-start"
-SSH_USER="mptcpdata"
-DATE=$(date +%Y%m%d_%H%M%S)
-
-echo "ssh $SSH_USER \"echo $DATE >> $DEST_FILE\""
-ssh $SSH_USER "echo $DATE >> $DEST_FILE"
